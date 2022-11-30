b34bda4e50425cc4fac7a0f3587d0e5833e30b19
lokalin <ilya-1974@yandex.ru>
2022-12-01 01:31:00 +0500

    feat: use publicpath for deploy

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 const config = {
   entry: './src/index.js',
+  output: {
+    path: path.resolve('public'),
+  },
   devServer: {
     open: true,
     host: 'localhost',
