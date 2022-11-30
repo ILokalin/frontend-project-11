da7bc1d2b626aaf4ae9289cf60e728c5b96d05c5
lokalin <ilya-1974@yandex.ru>
2022-12-01 00:51:25 +0500

    feat: add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+
+const config = {
+  entry: './src/index.js',
+  devServer: {
+    open: true,
+    host: 'localhost',
+    port: 5000,
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+};
+
+export default {
+  ...config,
+  mode: isProduction ? 'production' : 'development'
+};
